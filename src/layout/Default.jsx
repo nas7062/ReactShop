ecba205b1a80f3218c57abcd1d51a84b1de90cd7
10012 <nas7062@naver.com>
2025-04-22 17:45:31 +0900

    스크롤 상위로 이동 추가

--- src/layout/Default.jsx
+++ src/layout/Default.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../organism/Header'
 import Footer from '../organism/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import { Suspense } from 'react'
 const Default = () => {
   return (
@@ -10,6 +10,7 @@ const Default = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
+      <ScrollRestoration scrollBehavior="auto" />
       <Footer />
     </>
   )
